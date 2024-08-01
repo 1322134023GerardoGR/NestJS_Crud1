244245dc42ce7673c679891f1f80c413b672df77
GerardoGR <gerardo_1322134023@uptecamac.edu.mx>
2024-08-01 10:49:02 -0600

    build: create the project

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { Product } from './products/entity/product.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'products',
+      entities: [Product],
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
+})
+export class AppModule {}
